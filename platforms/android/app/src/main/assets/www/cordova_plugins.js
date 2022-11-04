4a4275ec0c8c63e47bf36a0d4eff31bb4d81ecb8
bigreni <santhanakrishnan.sai@gmail.com>
2022-11-03 21:58:32 -0500

    debug 1

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -37,6 +37,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "cordova.InAppBrowser.open"
       ]
+    },
+    {
+      "id": "cordova-plugin-admobpro.AdMob",
+      "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+      "pluginId": "cordova-plugin-admobpro",
+      "clobbers": [
+        "window.AdMob"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -46,6 +54,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-support-android-plugin": "2.0.4",
     "cordova-plugin-idfa": "2.1.0",
     "cordova-plugin-inappbrowser": "5.0.0",
-    "cordova-plugin-wkwebview-file-xhr": "3.1.0"
+    "cordova-plugin-wkwebview-file-xhr": "3.1.0",
+    "cordova-plugin-extension": "1.6.0",
+    "cordova-plugin-admobpro": "8.13.1"
   };
 });
