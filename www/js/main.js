2a5c3feb4e54ba27420b7a717368f6bfbcd76d92
bigreni <santhanakrishnan.sai@gmail.com>
2022-11-20 21:55:19 -0600

    Styling update

--- www/js/main.js
+++ www/js/main.js
@@ -262,34 +262,48 @@ function loadArrivals() {
                   else {
                       var direction = output['predictions'].direction;
                       results = results.concat("<p><strong>" + $("#routeSelect option:selected").text() + " - " + $("#routeStopSelect option:selected").text() + "</strong></p>");
+                      results = results.concat('<table id="tblResults" cellpadding="0" cellspacing="0">')
+                      results = results.concat('<tr class="header"><th>ROUTE</th><th>ARRIVAL</th></tr><tr><td class="spacer" colspan="2"></td></tr>');
                       if (direction == null) {
                           results = results.concat("<p> No upcoming arrivals.</p>");
                       }
                       else if (direction.length == null) {
                           if (output['predictions'].direction.prediction.length > 1) {
                               $.each(output['predictions'].direction.prediction, function (index, item) {
-                                  results = results.concat("<p>" + output['predictions'].direction.title + " - "  + item.minutes + " minutes </p>");
-                              });
+                                  //results = results.concat("<p>" + output['predictions'].direction.title + " - "  + item.minutes + " minutes </p>");
+                                  results = results.concat('<tr class="predictions">');
+                              results = results.concat("<td>" + output['predictions'].direction.title + "</td>"  + "<td>" + item.minutes + " minutes</td>");
+                              results = results.concat('</tr><tr><td class="spacer" colspan="2"></td></tr>');
+                            });
                           }
                           else {
-                              results = results.concat("<p>" + output['predictions'].direction.title + " - "  + output['predictions'].direction.prediction.minutes + " minutes </p>");
-                          }
+                            //   results = results.concat("<p>" + output['predictions'].direction.title + " - "  + output['predictions'].direction.prediction.minutes + " minutes </p>");
+                              results = results.concat('<tr class="predictions">');
+                              results = results.concat("<td>" + output['predictions'].direction.title + "</td>"  + "<td>" + output['predictions'].direction.prediction.minutes + " minutes</td></tr>");
+                            }
                       }
                       else{
-                            $.each(output['predictions'].direction, function (count, d) {
+                        $.each(output['predictions'].direction, function (count, d) {
                                 if (d.prediction.length > 1) {
                                     $.each(d.prediction, function (index, item) {
-                                    results = results.concat("<p>" + d.title + " - " + item.minutes + " minutes </p>");
+                                    //results = results.concat("<p>" + d.title + " - " + item.minutes + " minutes </p>");
+                                    results = results.concat('<tr class="predictions">');
+                                    results = results.concat("<td>" + d.title + "</td>"  + "<td>" + item.minutes  + " minutes</td>");
+                                    results = results.concat('</tr><tr><td class="spacer" colspan="2"></td></tr>');
                                     });
                                 }
                                 else {
-                                    results = results.concat("<p>" + d.title + " - " + d.prediction.minutes + " minutes </p>");
+                                    //results = results.concat("<p>" + d.title + " - " + d.prediction.minutes + " minutes </p>");
+                                    results = results.concat('<tr class="predictions">');
+                                    results = results.concat("<td>" + d.title + "</td>"  + "<td>" + d.prediction.minutes + " minutes</td></tr>");
+                                    
                                 }
                               });                          
                       }
                       if (results == "") {
                           results = results.concat("<p> No upcoming arrivals.</p>");
                       }
+                      results = results + "</table>";
                       $(outputContainer).html(results).show();
                       document.getElementById('btnSave').style.visibility = "visible";
                   }
